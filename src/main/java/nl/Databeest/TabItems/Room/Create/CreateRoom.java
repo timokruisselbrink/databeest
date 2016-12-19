e7dcf2feebfb473c1ca984d0aaf6eae72d436d32
Freek <freek.schraven@gmail.com>
2016-12-19 16:02:35 +0100

    get specifications en opslaan in een arraylist gemaakt

--- src/main/java/nl/Databeest/TabItems/Room/Create/CreateRoom.java
+++ src/main/java/nl/Databeest/TabItems/Room/Create/CreateRoom.java
@@ -4,13 +4,24 @@ import nl.Databeest.TabItems.SubMenuItem;
 
 import javax.swing.*;
 import java.awt.*;
+import java.sql.*;
+import java.util.ArrayList;
 
 /**
  * Created by timok on 15-12-16.
  */
 public class CreateRoom extends SubMenuItem {
     private JPanel mainPanel;
-    private JTextField textField1;
+    private JCheckBox combineCheckBox;
+    private JTextField roomNumberTextField;
+    private JTextField maxPersonsTextField;
+    private JTextField customPriceTextField;
+    private JTextField surfaceTextField;
+    private JComboBox roomTypeComboBox;
+    private JTextField floorTextField;
+    private JTable specificationsTable;
+
+
 
     @Override
     protected String getMenuItemName() {
@@ -21,4 +32,41 @@ public class CreateRoom extends SubMenuItem {
     protected Component getMainPanel() {
         return mainPanel;
     }
+
+    public CreateRoom(){
+        final ArrayList<String> selectedSpecifications = new ArrayList<String>();
+
+        specificationsTable.setModel(new selectSpecificationAbstractTableModel(getSpecifications()) {
+            @Override
+            public void addSelectedSpecification(String name) {
+                //write to specification_of_room
+
+                selectedSpecifications.add(name);
+
+            }
+        });
+
+        //roomTypeComboBox
+    }
+
+    public ResultSet getSpecifications() {
+        Connection con = getConnection();
+        ResultSet rs = null;
+
+
+        try {
+            PreparedStatement stmt = con.prepareStatement("SELECT NAME FROM SPECIFICATION" );
+            stmt.setEscapeProcessing(true);
+
+            rs=stmt.executeQuery();
+        }
+
+        catch (SQLException ex) {
+            ex.printStackTrace();
+            JOptionPane.showMessageDialog(null, ex.getMessage());
+        }
+        return rs;
+    }
+
+
 }
