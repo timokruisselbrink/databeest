34e4fe674b46643ff310cd98f0de49403a038a5a
A <arthur_baars@hotmail.com>
2017-01-12 16:30:31 +0100

    added refresh buttons

--- src/main/java/nl/Databeest/TabItems/Specification/addSpecification.java
+++ src/main/java/nl/Databeest/TabItems/Specification/addSpecification.java
@@ -20,17 +20,8 @@ public class addSpecification extends SubMenuItem {
     private JPanel mainPanel;
     private JButton btnAddSpecification;
     private JTextField txtAddSpecificationName;
-    private JPanel startDatePanel;
-    private JSpinner startDaySpinner;
-    private JComboBox startMonthComboBox;
-    private JSpinner startYearSpinner;
-    private JSpinner endDaySpinner;
-    private JComboBox endMonthComboBox;
-    private JSpinner endYearSpinner;
     private JTextField txtAddSpecificationPrice;
-    private JTextField txtAddSpecificationYear;
 
-    private final String[] MONTHS = new String[]{"January","February","March","April","May","June","July","August","September","October","November","December"};
 
     @Override
     protected String getMenuItemName() {
@@ -43,11 +34,6 @@ public class addSpecification extends SubMenuItem {
     }
 
     public addSpecification() {
-        startMonthComboBox.addItem("");
-
-        for (int i = 0; i < MONTHS.length; i++) {
-            startMonthComboBox.addItem(MONTHS[i]);
-            }
 
         btnAddSpecification.addActionListener(new ActionListener() {
             @Override
@@ -63,21 +49,13 @@ public class addSpecification extends SubMenuItem {
         PreparedStatement stmt = null;
 
 
-        Date startDate = DateHelper.createSqlDate(
-                startDaySpinner.getValue().toString(),
-                startMonthComboBox.getSelectedIndex(),
-                startYearSpinner.getValue().toString()
-        );
-
-
         try{
-            stmt = con.prepareStatement("SP_CREATE_SPECIFICATION ?,?,?");
+            stmt = con.prepareStatement("SP_CREATE_SPECIFICATION ?,?");
             stmt.setEscapeProcessing(true);
 
 
             stmt.setString(1, txtAddSpecificationName.getText());
-            stmt.setDate(2, startDate);
-            stmt.setFloat(3, Float.parseFloat(txtAddSpecificationPrice.getText()));
+            stmt.setFloat(2, Float.parseFloat(txtAddSpecificationPrice.getText()));
 
             stmt.execute();
             JOptionPane.showMessageDialog(null, "The specification has been added successfully.", "Success!", 1);
